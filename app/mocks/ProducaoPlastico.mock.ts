0a5150f23008abb1eeef3cc3c2341c11f602f0e1
Ellie de Oliveira <ellie.a.oliveira@gmail.com>
2024-06-06 19:02:21 -0300

    feat(BFG-42): inserir primeira tabela e criar pagina da solução

---
+++ app/mocks/ProducaoPlastico.mock.ts
@@ -0,0 +1,82 @@
+import { EntidadeId } from "../interfaces/Entidade.interface";
+import { ProducaoPlastico } from "../interfaces/ProducaoPlastico.interface";
+
+export const producaoPlasticoDataMock: ProducaoPlastico[] = [
+    {
+        ano: 2019,
+        producaoAnualPlastico: 459746020,
+        particiEmissaoOceanos: 0.248,
+        particiReciclagemRegional: 4.489,
+        particiQueimaRegional: 19.079,
+        particiLixoMalGerido: 3.553,
+        particiAterrosRegional: 72.88,
+        lixoMalGeridoPerCapita: 0.813,
+        entidadeId: 1 as EntidadeId
+    },
+    {
+        ano: 2019,
+        producaoAnualPlastico: 459746020,
+        particiEmissaoOceanos: 12.917,
+        particiReciclagemRegional: 13.333,
+        particiQueimaRegional: 4.161,
+        particiLixoMalGerido: 46.214,
+        particiAterrosRegional: 36.292,
+        lixoMalGeridoPerCapita: 9.51,
+        entidadeId: 2 as EntidadeId
+    },
+    {
+        ano: 2019,
+        producaoAnualPlastico: 459746020,
+        particiEmissaoOceanos: 0.187,
+        particiReciclagemRegional: 8.351,
+        particiQueimaRegional: 18.514,
+        particiLixoMalGerido: 33.746,
+        particiAterrosRegional: 39.389,
+        lixoMalGeridoPerCapita: 0.281,
+        entidadeId: 3 as EntidadeId
+    },
+    {
+        ano: 2019,
+        producaoAnualPlastico: 459746020,
+        particiEmissaoOceanos: 0.014,
+        particiReciclagemRegional: 12.384,
+        particiQueimaRegional: 37.747,
+        particiLixoMalGerido: 5.668,
+        particiAterrosRegional: 44.201,
+        lixoMalGeridoPerCapita: 0.607,
+        entidadeId: 4 as EntidadeId
+    },
+    {
+        ano: 2019,
+        producaoAnualPlastico: 459746020,
+        particiEmissaoOceanos: 0.003,
+        particiReciclagemRegional: 6.871,
+        particiQueimaRegional: 11.508,
+        particiLixoMalGerido: 6.755,
+        particiAterrosRegional: 74.866,
+        lixoMalGeridoPerCapita: 0.209,
+        entidadeId: 5 as EntidadeId
+    },
+    {
+        ano: 2019,
+        producaoAnualPlastico: 459746020,
+        particiEmissaoOceanos: 0.024,
+        particiReciclagemRegional: 12.384,
+        particiQueimaRegional: 37.747,
+        particiLixoMalGerido: 5.668,
+        particiAterrosRegional: 44.201,
+        lixoMalGeridoPerCapita: 0.435,
+        entidadeId: 6 as EntidadeId
+    },
+    {
+        ano: 2019,
+        producaoAnualPlastico: 459746020,
+        particiEmissaoOceanos: 0.04,
+        particiReciclagemRegional: 8.351,
+        particiQueimaRegional: 18.514,
+        particiLixoMalGerido: 33.746,
+        particiAterrosRegional: 39.389,
+        lixoMalGeridoPerCapita: 0.237,
+        entidadeId: 7 as EntidadeId
+    }
+];
--- app/solucao/page.tsx
+++ app/solucao/page.tsx
@@ -1,13 +1,54 @@
 "use client"
 
+import { useEffect, useState } from "react"
 import { AuthGuard } from "../components"
+import { ProducaoPlastico } from "../interfaces/ProducaoPlastico.interface"
+import { PoluicaoCidade } from "../interfaces/PoluicaoCidade.interface"
+import camelCaseToHumanReadable from "../helper/camelcase-to-human-readable"
+import { producaoPlasticoDataMock } from "../mocks/ProducaoPlastico.mock"
 
 export default function Solucao() {
+    const [dadosProducaoPlastico, setDadosProducaoPlastico] = useState<ProducaoPlastico[]>([])
+    const [dadosPoluicaoCidade, setDadosPoluicaoCidade] = useState<PoluicaoCidade[]>([])
+
+    useEffect(() => {
+        // TODO - consumir a API e popular as tabelas
+        setDadosProducaoPlastico(
+            producaoPlasticoDataMock
+        )
+    }, [])
+    // TODO - pegar dados da API e popular as tabelas
+
     return (
         <AuthGuard>
-        <div>
-            <h2>Solução</h2>
-        </div>
+            <main className="lg:mx-16 xl:mx-48 mt-20">
+                <article className="h-screen">
+                    <header>
+                        <h2 className="text-5xl font-bold tracking-tight text-white">Produção de Plástico</h2>
+                    </header>
+                    <section className="mt-4">
+                        <table className="min-w-full border-collapse block md:table text-white">
+                            <thead className="block md:table-header-group bg-primary">
+                                <tr className="border border-gray md:border-none block md:table-row">
+                                    {dadosProducaoPlastico.length > 0 &&
+                                        Object.keys(dadosProducaoPlastico[0]).map((key, index) =>
+                                            <th className="bg-gray-200 p-2 text-gray-600 text-left block md:table-cell" key={index}>{camelCaseToHumanReadable(key)}</th>
+                                        )}
+                                </tr>
+                            </thead>
+                            <tbody className="block md:table-row-group">
+                                {dadosProducaoPlastico.map((producaoPlastico, index) => (
+                                    <tr className="bg-secondary border border-gray md:border-none block md:table-row" key={index}>
+                                        {Object.values(producaoPlastico).map((value, index) =>
+                                            <td className="p-2 text-gray-600 text-left block md:table-cell" key={index}>{value}</td>
+                                        )}
+                                    </tr>
+                                ))}
+                            </tbody>
+                        </table>
+                    </section>
+                </article>
+            </main>
         </AuthGuard>
     )
 }
