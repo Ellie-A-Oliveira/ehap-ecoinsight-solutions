16a696bdba01a71d537183ab6696db7e19e0c5bc
Ellie de Oliveira <ellie.a.oliveira@gmail.com>
2024-06-06 19:20:56 -0300

    feat(BFG-42): extrair componente table

---
+++ app/components/Table/Table.tsx
@@ -0,0 +1,43 @@
+import camelCaseToHumanReadable from "@/app/helper/camelcase-to-human-readable"
+
+interface Table {
+    data: Object[]
+    title: string
+    classList?: string
+    loadingData?: boolean
+}
+
+export const Table = ({ data, title, classList, loadingData }: Table) => {
+    return <article className={`${classList}`}>
+        <header>
+            <h2 className="text-5xl font-bold tracking-tight text-white">{title}</h2>
+        </header>
+        <section className="mt-4">
+            {
+                loadingData ? <p className="text-white">Carregando...</p>
+                :
+                !loadingData && data.length === 0 ? 
+                <p className="text-white">Nenhum dado a exibir.</p>
+                :
+                <table className="min-w-full border-collapse block md:table text-white">
+                    <thead className="block md:table-header-group bg-primary">
+                        <tr className="border border-gray md:border-none block md:table-row">
+                            {Object.keys(data[0]).map((key, index) =>
+                                <th className="bg-gray-200 p-2 text-gray-600 text-left block md:table-cell" key={index}>{camelCaseToHumanReadable(key)}</th>
+                            )}
+                        </tr>
+                    </thead>
+                    <tbody className="block md:table-row-group">
+                        {data.map((row, index) => (
+                            <tr className="bg-secondary border border-gray md:border-none block md:table-row" key={index}>
+                                {Object.values(row).map((value, index) =>
+                                    <td className="p-2 text-gray-600 text-left block md:table-cell" key={index}>{value}</td>
+                                )}
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
+            }
+        </section>
+    </article>
+}
--- app/solucao/page.tsx
+++ app/solucao/page.tsx
@@ -1,53 +1,43 @@
 "use client"
 
 import { useEffect, useState } from "react"
-import { AuthGuard } from "../components"
+import { AuthGuard, Table } from "../components"
 import { ProducaoPlastico } from "../interfaces/ProducaoPlastico.interface"
 import { PoluicaoCidade } from "../interfaces/PoluicaoCidade.interface"
-import camelCaseToHumanReadable from "../helper/camelcase-to-human-readable"
 import { producaoPlasticoDataMock } from "../mocks/ProducaoPlastico.mock"
 
 export default function Solucao() {
     const [dadosProducaoPlastico, setDadosProducaoPlastico] = useState<ProducaoPlastico[]>([])
     const [dadosPoluicaoCidade, setDadosPoluicaoCidade] = useState<PoluicaoCidade[]>([])
 
+    const [loadingProducaoPlastico, setLoadingProducaoPlastico] = useState(true)
+    const [loadingPoluicaoCidade, setLoadingPoluicaoCidade] = useState(true)
+
     useEffect(() => {
         // TODO - consumir a API e popular as tabelas
         setDadosProducaoPlastico(
             producaoPlasticoDataMock
         )
+
+        setTimeout(() => {
+            setLoadingProducaoPlastico(false)
+        }, 3000)
     }, [])
-    // TODO - pegar dados da API e popular as tabelas
+
+    useEffect(() => {
+        // TODO - consumir a API e popular as tabelas
+        setDadosPoluicaoCidade([])
+
+        setTimeout(() => {
+            setLoadingPoluicaoCidade(false)
+        }, 3000)
+    }, [])
 
     return (
         <AuthGuard>
             <main className="lg:mx-16 xl:mx-48 mt-20">
-                <article className="h-screen">
-                    <header>
-                        <h2 className="text-5xl font-bold tracking-tight text-white">Produção de Plástico</h2>
-                    </header>
-                    <section className="mt-4">
-                        <table className="min-w-full border-collapse block md:table text-white">
-                            <thead className="block md:table-header-group bg-primary">
-                                <tr className="border border-gray md:border-none block md:table-row">
-                                    {dadosProducaoPlastico.length > 0 &&
-                                        Object.keys(dadosProducaoPlastico[0]).map((key, index) =>
-                                            <th className="bg-gray-200 p-2 text-gray-600 text-left block md:table-cell" key={index}>{camelCaseToHumanReadable(key)}</th>
-                                        )}
-                                </tr>
-                            </thead>
-                            <tbody className="block md:table-row-group">
-                                {dadosProducaoPlastico.map((producaoPlastico, index) => (
-                                    <tr className="bg-secondary border border-gray md:border-none block md:table-row" key={index}>
-                                        {Object.values(producaoPlastico).map((value, index) =>
-                                            <td className="p-2 text-gray-600 text-left block md:table-cell" key={index}>{value}</td>
-                                        )}
-                                    </tr>
-                                ))}
-                            </tbody>
-                        </table>
-                    </section>
-                </article>
+                <Table data={dadosProducaoPlastico} title="Produção Anual de Plastico" loadingData={loadingProducaoPlastico} />
+                <Table classList="mt-8" data={dadosPoluicaoCidade} title="Poluição por Cidade" loadingData={loadingPoluicaoCidade} />
             </main>
         </AuthGuard>
     )
